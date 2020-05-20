8ed98ac0a8b017ecce5e0fb1c0830fb7deb313e0
onuki0205 <aizu1260027@gmail.com>
2020-05-20 14:36:39 +0900

    add

--- ex07/BankTest.java
+++ ex07/BankTest.java
@@ -4,8 +4,19 @@ public class BankTest {
 
 
 	public static void main(String args[]) {
+		Bank bank = new Bank(N_ACCOUNTS, INIT_BALANCE);
+		Transfer[] transfer = new Transfer[N_ACCOUNTS];
+		Thread[] thread = new Thread[N_ACCOUNTS]; 
 	
 		// COMPLETE
+		for(int i = 0; i < N_ACCOUNTS; i++){
+			transfer[i] = new Transfer(bank, i, INIT_BALANCE);
+		}
+
+		for(int j = 0; j < N_ACCOUNTS; j++){
+			thread[j] = new Thread(transfer[j]);
+			thread[j].start();
+		}
 		
 	}
 }
--- ex07/Transfer.java
+++ ex07/Transfer.java
@@ -1,3 +1,4 @@
+import java.util.Random;
 
 // COMPLETE
 //
@@ -5,7 +6,8 @@
 // and transfer it to a random account in the bank.
 //
 
-public class Transfer {
+public class Transfer implements Runnable{
+	Random random = new Random();
 	public Transfer(Bank bank, int from, int max_amount) {
 		bank_ = bank;
 		from_ = from;
@@ -17,4 +19,14 @@ public class Transfer {
 	private Bank bank_;
 	private int from_;
 	private int max_;
+
+	public void run(){
+		while(true){
+			int to_ = random.nextInt(BankTest.N_ACCOUNTS);
+			int amount = random.nextInt(this.max_);
+    		this.bank_.transfer(this.from_, to_, amount);
+		  }
+	}
+
+
 }
