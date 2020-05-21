9b94dd5c8b020abf06cb9100e74a9952c3670c94
onuki0205 <41815571+onuki0205@users.noreply.github.com>
2020-05-21 09:20:50 +0900

    fix

--- ex08/SimpleHTTPServerのコピー.java
+++ ex08/SimpleHTTPServerのコピー.java
@@ -16,16 +16,13 @@ public class SimpleHTTPServer {
 
   public void start() {
     // TODO
-    //ServerSocket serverSocket = new ServerSocket(this.port);
-    Socket sock = null;//finallyで閉じるためにtry外で宣言
+    ServerSocket serverSocket = new ServerSocket(this.port);
+    Socket sock = null;
     ServerSocket serverSock = null;
-    try {//例外が発生する可能性が非常に高いのでtry/catchブロックを使う 教科書p481
-      //サーバプログラムはServerSocketでクライアントからのリクエスト(クライアントでSocketオブジェクトが作られる時に送られる)ものに対応
+    try {
       serverSock = new ServerSocket(this.port);
 
       while(true) {
-        // 接続を確認したらソケットを作成
-        //accept()メソッドはリクエストがあるまで何もしない。リクエストがあれば、クライアントとの通信のためのSocketオブジェクトを戻す
         sock = serverSock.accept();
         Handler handle = new Handler(sock);
         handle.start();
@@ -84,18 +81,16 @@ public class SimpleHTTPServer {
           if (reqMethod.equals("GET")) {
 
             String fileName = req[1];//２単語目 filenameに対応
-            fileName = fileName.replaceAll("^/", ""); // /を消す
+            fileName = fileName.replaceAll("^/", "");
             File file = new File(fileName);
             //System.out.println(fileName);
             byte [] content;
             if(file.exists()){
-              //Before sending the file content, you need to prepend an HTTP header that contains information on the content length, the encoding, etc.
-              //ファイルコンテンツを送信する前に、コンテンツの長さ、エンコードなどに関する情報を含むHTTPヘッダーを追加する必要がある。
-              //template
-              content = Files.readAllBytes(file.toPath());//File型をPath型に変換して指定のパスのファイルの中身をバイト列として全部読み込む
+
+              content = Files.readAllBytes(file.toPath());
             }else{
               File file404 = new File("404.html");
-              content = Files.readAllBytes(file404.toPath());//File型をPath型に変換して指定のパスのファイルの中身をバイト列とし
+              content = Files.readAllBytes(file404.toPath());
             }
             String headerStr = "HTTP/1.0 200 OK\r\n"
             + "Server: SimpleHTTPServer\r\n"
@@ -104,11 +99,9 @@ public class SimpleHTTPServer {
             + "; charset=utf-8" + "\r\n\r\n";
             byte[] header = headerStr.getBytes(Charset.forName("UTF-8"));
 
-            // send the header then the content as byte[] via the OutputStream
             out.write(header, 0, header.length);
             out.write(content, 0, content.length);
-            //Java言語の System.out では，効率化のため，改行が来るまでデータを内部に溜めておき，改行が来たらまとめて出力するしくみになっています。 そこで，行の途中で強制出力
-            out.flush();// 強制出力
+            out.flush();
           }
         }
       }catch(IOException e){
